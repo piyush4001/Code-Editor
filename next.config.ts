be342425b54ff6843eae38ace8cadd5b40165b7d
piyush4001 <piyushsoni8839@gmail.com>
2026-02-03 00:09:20 +0530

    next.config.ts changes for cross cors origin sites

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,56 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   experimental: {},
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "*",
+//         port: "",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+//   async headers() {
+//     return [
+//       {
+//         // Apply to all routes
+//         source: "/:path*",
+//         headers: [
+//           {
+//             key: "Cross-Origin-Opener-Policy",
+//             value: "same-origin",
+//           },
+//           {
+//             key: "Cross-Origin-Embedder-Policy",
+//             value: "require-corp",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+//   reactStrictMode: false,
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {},
+  reactStrictMode: false,
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*",
-        port: "",
-        pathname: "/**",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
   },
+
   async headers() {
     return [
       {
-        // Apply to all routes
         source: "/:path*",
         headers: [
           {
@@ -30,7 +65,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  reactStrictMode: false,
 };
 
 export default nextConfig;
